d700b29430b1c6980f241c1c0e2d286b0a32a417
Irina Aleksandrova <myr-irina2@yandex.ru>
2021-08-21 11:31:26 +0300

    feat: объединила бэкенд и фронтэнд

---
+++ backend/app.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const helmet = require('helmet');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const { errors } = require('celebrate');
+const usersRouter = require('./routes/users');
+const cardsRouter = require('./routes/cards');
+const auth = require('./middlewares/auth');
+const { createUser, login } = require('./controllers/users');
+const errorHandler = require('./middlewares/errorHandler');
+require('dotenv').config();
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+const { PORT = 3000 } = process.env;
+const app = express();
+const { validateSignUp, validateSignIn } = require('./middlewares/validators');
+const NotFoundError = require('./errors/NotFoundError');
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.use(helmet());
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
+app.use(cookieParser());
+
+app.use(requestLogger);
+
+app.post('/signup', validateSignUp, createUser);
+app.post('/signin', validateSignIn, login);
+
+app.use(auth);
+
+app.use('/users', usersRouter);
+app.use('/cards', cardsRouter);
+app.use('*', (req, res, next) => {
+  next(new NotFoundError('Запрашиваемый ресурс не найден.'));
+});
+
+app.use(errorLogger);
+
+app.use(errors()); // обработчик ошибок celebrate
+app.use(errorHandler); // централизованный обработчик ошибок
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
