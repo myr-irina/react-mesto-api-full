9f9ee48095b10ea0af56a8fd7681b7f58014c6f2
Irina Aleksandrova <myr-irina2@yandex.ru>
2021-08-24 21:17:18 +0300

    fix: CORS

--- backend/app.js
+++ backend/app.js
@@ -28,6 +28,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 app.use(cors({
   origin: [
     'https://mesto.mesto.students.nomoredomains.monster',
+    'http://mesto.mesto.students.nomoredomains.monster',
   ],
 }));
 app.use(helmet());
